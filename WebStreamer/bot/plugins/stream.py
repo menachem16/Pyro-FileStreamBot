c0f6c5cc9aa287c76a4bb91cabc075b751f0e799
kingtvland <42252946+menachem16@users.noreply.github.com>
2022-11-17 20:14:31 +0200

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -73,7 +73,8 @@ async def private_receive_handler(c: Client, m: Message):
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
         file_name = get_media_file_name(m)
-        file_name1 = file_name.
+        file_name1 = file_name.text
+        print('file_name -',file_name1)
         file_size = humanbytes(get_media_file_size(m))
         stream_link = "https://{}/{}/{}".format(Var.FQDN, log_msg.message_id, file_name) if Var.ON_HEROKU or Var.NO_PORT else \
             "http://{}:{}/{}/{}".format(Var.FQDN,
