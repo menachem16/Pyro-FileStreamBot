c511d7c0364894f6eceb6adf22592c9d2876c9e1
kingtvland <42252946+menachem16@users.noreply.github.com>
2022-11-17 20:46:28 +0200

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -11,7 +11,7 @@ from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
 
-params = ""
+params = []
 def get_media_file_size(m):
     media = m.video or m.audio or m.document
     if media and media.file_size:
